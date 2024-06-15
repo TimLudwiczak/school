ea9d2c013586887116bb7dd530729078447e2d1f
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-15 11:42:50 -0500

    idk

--- classes/school.py
+++ classes/school.py
@@ -16,3 +16,11 @@ class School:
         for student in self.students:
             if student.school_id == student_id:
                 return student
+    def add_student(self):
+        student_data = {'role':'student'}
+        student_data['name']      = input('Enter student name:\n')
+        student_data['age']       = input('Enter student age: \n')
+        student_data['school_id'] = input('Enter student school id: \n')
+        student_data['password']  = input('Enter student password: \n')
+        student = Student(**student_data)
+        self.students.append(student)
